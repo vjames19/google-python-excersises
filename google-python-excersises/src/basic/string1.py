29cc8e73edbbdf61be228a245b1da7f9f6c08730
Victor J <victor.reventos1@gmail.com>
2012-08-22 16:28:58 -0400

    Finished string1.py

--- google-python-excersises/src/basic/string1.py
+++ google-python-excersises/src/basic/string1.py
@@ -59,11 +59,14 @@ def fix_start(s):
 # by a space '<a> <b>', except swap the first 2 chars of each string.
 # e.g.
 #   'mix', pod' -> 'pox mid'
-#   'dog', 'dinner' -> 'dig donner'
+   'dog', 'dinner' -> 'dig donner'
 # Assume a and b are length 2 or more.
 def mix_up(a, b):
   # +++your code here+++
-  return
+   a_swapped = b[:2] + a[2:]
+  b_swapped = a[:2] + b[2:]
+  return a_swapped + ' ' + b_swapped
+  
 
 
 # Provided simple test() function used in main() to print
